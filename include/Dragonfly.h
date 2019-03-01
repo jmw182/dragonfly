65d30af3ddd90d91ea1eeb7c767f16c73b3989a5
Jeff Weiss <weiss.jeffrey.m@gmail.com>
2019-03-01 16:32:41 -0500

    Removed a multi-line macro definition (by commenting out and copying/pasting values) for python3 compatibility

--- include/Dragonfly.h
+++ include/Dragonfly.h
@@ -35,7 +35,18 @@ class CMessage
 {
 public:
 
-	DF_MSG_HEADER_FIELDS;
+	MSG_TYPE	msg_type; // previously DF_MSG_HEADER_FIELDS; (changed for python compatibility)
+	MSG_COUNT	msg_count;
+	double	send_time;
+	double	recv_time;
+	HOST_ID		src_host_id;
+	MODULE_ID	src_mod_id;
+	HOST_ID		dest_host_id;
+	MODULE_ID	dest_mod_id;
+	int			num_data_bytes;
+	int			remaining_bytes;
+	int			is_dynamic;
+	int			reserved;
 	char data[MAX_CONTIGUOUS_MESSAGE_DATA];
 	char *large_data;
 
--- include/Dragonfly_types.h
+++ include/Dragonfly_types.h
@@ -22,19 +22,21 @@ typedef int MSG_COUNT;
 #define MAX_DRAGONFLY_MODULE_ID  9 // Module ID-s below 10 are reserved for Dragonfly core
 
 // Header fields for all messages passed through Dragonfly
-#define DF_MSG_HEADER_FIELDS \
-	MSG_TYPE	msg_type; \
-	MSG_COUNT	msg_count; \
-	double	send_time; \
-	double	recv_time; \
-	HOST_ID		src_host_id; \
-	MODULE_ID	src_mod_id; \
-	HOST_ID		dest_host_id; \
-	MODULE_ID	dest_mod_id; \
-	int			num_data_bytes; \
-	int			remaining_bytes; \
-	int			is_dynamic; \
-	int			reserved
+// Following macro was commented out for compatibility with ctypesgen2 v2.2.2 python package (used for Python3 compatiblity) 
+// All references to this macro in this file and in Dragonfly.h were replaced with actual values
+//#define DF_MSG_HEADER_FIELDS
+	// MSG_TYPE	msg_type;
+	// MSG_COUNT	msg_count;
+	// double	send_time;
+	// double	recv_time;
+	// HOST_ID		src_host_id;
+	// MODULE_ID	src_mod_id;
+	// HOST_ID		dest_host_id;
+	// MODULE_ID	dest_mod_id;
+	// int			num_data_bytes;
+	// int			remaining_bytes;
+	// int			is_dynamic;
+	// int			reserved
 // msg_type - Message type ID
 // msg_count - Source message count (per source, starting from 1)
 // send_time - Time at source when message sent (Seconds since 0:00:00 on Jan 1, 1970)
@@ -52,13 +54,35 @@ typedef int MSG_COUNT;
 
 // Header for messages passed through Dragonfly
 typedef struct {
-	DF_MSG_HEADER_FIELDS;
+	MSG_TYPE	msg_type; // previously DF_MSG_HEADER_FIELDS; (changed for python compatibility)
+	MSG_COUNT	msg_count;
+	double	send_time;
+	double	recv_time;
+	HOST_ID		src_host_id;
+	MODULE_ID	src_mod_id;
+	HOST_ID		dest_host_id;
+	MODULE_ID	dest_mod_id;
+	int			num_data_bytes;
+	int			remaining_bytes;
+	int			is_dynamic;
+	int			reserved;
 } DF_MSG_HEADER;
 
 #define MAX_CONTIGUOUS_MESSAGE_DATA 9000
 
 typedef struct {
-	DF_MSG_HEADER_FIELDS;
+	MSG_TYPE	msg_type; // previously DF_MSG_HEADER_FIELDS; (changed for python compatibility)
+	MSG_COUNT	msg_count;
+	double	send_time;
+	double	recv_time;
+	HOST_ID		src_host_id;
+	MODULE_ID	src_mod_id;
+	HOST_ID		dest_host_id;
+	MODULE_ID	dest_mod_id;
+	int			num_data_bytes;
+	int			remaining_bytes;
+	int			is_dynamic;
+	int			reserved;
 	char data[MAX_CONTIGUOUS_MESSAGE_DATA];
 } DF_MESSAGE;
 
