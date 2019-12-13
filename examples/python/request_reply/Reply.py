6701b02e2c631aa6f32d37ef20ab2a091d075c89
Tyler Madonna <tjm159@pitt.edu>
2019-12-13 15:14:52 -0500

    Updated request reply example for python 3

--- examples/python/request_reply/Reply.py
+++ examples/python/request_reply/Reply.py
@@ -1,36 +1,51 @@
 #!/usr/bin/python
 import time
-import PyDragonfly
-from PyDragonfly import copy_from_msg, copy_to_msg, MT_EXIT, MT_KILL
-import message_defs as mdefs
 import sys
+
+import message_defs as md
+
+# Python version conditional import.
+# Not necessary if targeting single version of Python
+if sys.version_info[0] == 3 and sys.version_info[1] == 7:
+    # Python 3.7
+	import PyDragonfly3 as df
+	from PyDragonfly3 import copy_from_msg, copy_to_msg, MT_EXIT, MT_KILL
+
+elif sys.version_info[0] == 2 and sys.version_info[1] == 7:
+    # Python 2.7
+	import PyDragonfly2 as df
+	from PyDragonfly2 import copy_from_msg, copy_to_msg, MT_EXIT, MT_KILL
+
+else:
+	raise ImportError("Consumer only supports Python 2.7 and 3.7")
+
 
 MID_REPLY = 10
 
 # Note: Reply must be started first
 
 if __name__ == "__main__":
-    mod = PyDragonfly.Dragonfly_Module(MID_REPLY, 0)
+    mod = df.Dragonfly_Module(MID_REPLY, 0)
     mod.ConnectToMMM()
-    mod.Subscribe(mdefs.MT_REQUEST_TEST_DATA)
+    mod.Subscribe(md.MT_REQUEST_TEST_DATA)
     
-    print "Reply running...\n"
+    print("Reply running...\n")
     
     run = True
     while run:
-        in_msg = PyDragonfly.CMessage()
-        print "Waiting for message"
+        in_msg = df.CMessage()
+        print("Waiting for message")
         rcv = mod.ReadMessage(in_msg, 1.0)
         if rcv == 1:
-            print "Received message", in_msg.GetHeader().msg_type
-            out_msg = PyDragonfly.CMessage(mdefs.MT_TEST_DATA)
-            data = mdefs.MDF_TEST_DATA()
-            if in_msg.GetHeader().msg_type == mdefs.MT_REQUEST_TEST_DATA:
+            print("Received message", in_msg.GetHeader().msg_type)
+            out_msg = df.CMessage(md.MT_TEST_DATA)
+            data = md.MDF_TEST_DATA()
+            if in_msg.GetHeader().msg_type == md.MT_REQUEST_TEST_DATA:
                 data.a = -20
                 data.b = 47
                 data.x = 123.456
                 copy_to_msg(data, out_msg)
                 mod.SendMessage(out_msg)
-                print "Sent message", out_msg.GetHeader().msg_type
+                print("Sent message", out_msg.GetHeader().msg_type)
             elif in_msg.GetHeader().msg_type in (MT_EXIT, MT_KILL):
                run = False        
--- examples/python/request_reply/Request.py
+++ examples/python/request_reply/Request.py
@@ -1,32 +1,47 @@
 #!/usr/bin/python
 import time
-import PyDragonfly
-from PyDragonfly import copy_from_msg, copy_to_msg, MT_EXIT, MT_KILL
-import message_defs as mdefs
 import sys
+
+import message_defs as md
+
+# Python version conditional import.
+# Not necessary if targeting single version of Python
+if sys.version_info[0] == 3 and sys.version_info[1] == 7:
+    # Python 3.7
+	import PyDragonfly3 as df
+	from PyDragonfly3 import copy_from_msg, copy_to_msg, MT_EXIT, MT_KILL
+
+elif sys.version_info[0] == 2 and sys.version_info[1] == 7:
+    # Python 2.7
+	import PyDragonfly2 as df
+	from PyDragonfly2 import copy_from_msg, copy_to_msg, MT_EXIT, MT_KILL
+
+else:
+	raise ImportError("Consumer only supports Python 2.7 and 3.7")
+
 
 MID_REQUEST = 11
 
 # Note: Request must be started second
 
 if __name__ == "__main__":
-    mod = PyDragonfly.Dragonfly_Module(MID_REQUEST, 0)
+    mod = df.Dragonfly_Module(MID_REQUEST, 0)
     mod.ConnectToMMM()
-    mod.Subscribe(mdefs.MT_TEST_DATA)
+    mod.Subscribe(md.MT_TEST_DATA)
     mod.Subscribe(MT_EXIT)
     
-    print "Request running...\n"
+    print("Request running...\n")
     
     while ( 1):
-        mod.SendSignal(mdefs.MT_REQUEST_TEST_DATA)
+        mod.SendSignal(md.MT_REQUEST_TEST_DATA)
         print("Sent request for data")
-        msg = PyDragonfly.CMessage()
+        msg = df.CMessage()
         mod.ReadMessage(msg)
-        print "Received message", msg.GetHeader().msg_type
-        if msg.GetHeader().msg_type == mdefs.MT_TEST_DATA:
-            msg_data = mdefs.MDF_TEST_DATA()
+        print("Received message", msg.GetHeader().msg_type)
+        if msg.GetHeader().msg_type == md.MT_TEST_DATA:
+            msg_data = md.MDF_TEST_DATA()
             copy_from_msg(msg_data, msg)
-            print "Data = [a: %d, b: %d, x: %f]" % (msg_data.a, msg_data.b, msg_data.x)
+            print("Data = [a: %d, b: %d, x: %f]" % (msg_data.a, msg_data.b, msg_data.x))
         time.sleep(1)
         
     mod.DisconnectFromMMM()
