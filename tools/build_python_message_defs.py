9004a92a9d1121ea1270a134e945f800ef454121
Tyler Madonna <tjm159@pitt.edu>
2019-12-13 15:06:42 -0500

    Updated build_python_message_defs.py to use clang2py for Windows

--- tools/build_python_message_defs.py
+++ tools/build_python_message_defs.py
@@ -1,28 +1,28 @@
-
 import os
 import platform
 import subprocess as sp
 from argparse import ArgumentParser
 
-if __name__ == "__main__":
-    parser = ArgumentParser(description = "Converts C header files to python files")
+if __name__ == '__main__':
+    parser = ArgumentParser(description = 'Converts C header files to python files')
     parser.add_argument(type=str, dest='input_file')
     args = parser.parse_args()
-    print("Processing %s ..." % (args.input_file))
+    print('Processing %s ...' % (args.input_file))
 
     input_file = os.path.abspath(args.input_file)
     base_dir, filename = os.path.split(input_file)
     filename_root, filename_ext = os.path.splitext(filename)
     
     os_name = platform.system()
-    if os_name == "Linux":
+    if os_name == 'Linux':
+        # This needs updated to call clang2py
         xml_file = os.path.join(base_dir, filename_root) + '.xml'
         sp.call(['h2xml', '-c', '-o', xml_file, input_file])
         out_file = os.path.join(base_dir, filename_root) + '.py'
         sp.call(['xml2py', '-o', out_file, xml_file])
 
-    elif os_name == "Windows":
-        ctypesgen_path = os.environ['CTYPESGEN'] + '\ctypesgen.py'
+    elif os_name == 'Windows':
         output_file = os.path.join(base_dir, filename_root) + '.py'
-        sp.call(['python', ctypesgen_path, '--includedir="../include"', '-a', '-o', output_file, input_file])
-        
+        python_base = os.environ['PYTHON3_BASE']
+        clang2py = os.path.join(python_base, 'Lib\site-packages\ctypeslib\clang2py.py')
+        sp.call(['python', clang2py, '-c', '-k', 'cdefmstu', '-o', output_file, input_file])
